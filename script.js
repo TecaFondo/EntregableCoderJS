0ebbfce9a657a1136a0cf3231fbf73d308e07f53
TecaFondo <carlos.kohler.r@mail.pucv.cl>
2023-04-24 21:22:50 -0400

    Se adapta index.html y se agrega microdesafio de JS

---
+++ script.js
@@ -0,0 +1,16 @@
+let nombre = prompt("¿Cuál es tu nombre?");
+
+alert(`Hola ${nombre}, bienvenido!`);
+console.log(`Hola ${nombre}, bienvenido!`);
+
+let num1 = parseInt(prompt("Ingrese un número"));
+let num2 = parseInt(prompt("Ingrese otro número"));
+
+alert(`La suma de los números es ${num1 + num2}`);
+console.log(`La suma de los números es ${num1 + num2}`);
+
+let historia1 = prompt("Ingrese una historia");
+let historia2 = prompt("Ingrese otra historia");
+
+console.log(`Ambas historias juntas serían: ${historia1} ${historia2}`);
+alert(`Ambas historias juntas serían: ${historia1} ${historia2}`);
