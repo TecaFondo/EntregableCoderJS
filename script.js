192e6d78e70cf1fc83fb13e5418df201c5ce6386
TecaFondo <carlos.kohler.r@mail.pucv.cl>
2023-05-21 20:31:09 -0400

    Se modifica visual de pagina

--- script.js
+++ script.js
@@ -45,6 +45,8 @@ function getEdad(){
 */
 
 
+
+
 // Obtener el elemento donde se mostrarán los datos
 let personasContainer = document.getElementById('productos');
 
@@ -59,26 +61,61 @@ fetch('productos.json')
     // Recorrer cada persona y agregarla al HTML
     personas.forEach(function(persona) {
         let nombre = persona.nombre;
-        let edad = persona.edad;
+        let precio = persona.precio;
         let foto = persona.img;
+        let pantalla = persona.pantalla;
+        let procesador = persona.procesador;
+        let cpu = persona.cpu;
+        let grafica = persona.grafica;
 
         // Crear un elemento de párrafo para mostrar los datos
-        
         let card = document.createElement('div');
-        card.classList.add('card');
-        let p = document.createElement('p');
-        p.classList.add('card-text');
-        let p2 = document.createElement('p');
-        p.textContent = nombre;
-        p2.textContent = 'Edad: ' + edad;
+        card.classList.add('card'); //Agrega la clase card al div
+        let prodNom = document.createElement('p');
+        prodNom.classList.add('card-text'); //Agrega la clase card-text al párrafo
+        prodNom.classList.add('comparador');//Agrega la clase comparador al párrafo
+        let prodPrec = document.createElement('p');
+        prodPrec.classList.add('precio');//Agrega la clase precio al párrafo
+        let prodPant = document.createElement('p');
+        prodPant.classList.add('pantalla');//Agrega la clase pantalla al párrafo
+        let prodProc = document.createElement('p');
+        prodProc.classList.add('procesador');//Agrega la clase procesador al párrafo
+        let prodCpu = document.createElement('p');
+        prodCpu.classList.add('cpu');//Agrega la clase cpu al párrafo
+        let prodGraf = document.createElement('p');
+        prodGraf.classList.add('grafica');//Agrega la clase grafica al párrafo
+
+        //Se genra boton de comprar
+        let boton = document.createElement('button');
+        boton.classList.add('btn');
+        boton.classList.add('btn-primary');
+        boton.classList.add('btn-comprar');
+        boton.classList.add('comprarComparador');
+        
+
+        // Se asignan los valores a los elementos
+        prodNom.textContent = nombre;
+        prodPrec.textContent =  precio;
         let imagen = document.createElement('img');
         imagen.src = foto;
-        console.log('persona.imagen '+persona.imagen);
-        // Agregar el elemento de párrafo al contenedor
+        prodPant.textContent = pantalla+'"';
+        prodProc.textContent = procesador;
+        prodCpu.textContent = cpu;
+        prodGraf.textContent = grafica;
+        boton.textContent = "Comprar";
+
+        // Agrega los elementos al contenedor
         personasContainer.appendChild(card);
         card.appendChild(imagen);
-        card.appendChild(p);
-        card.appendChild(p2);
+        card.appendChild(prodNom);
+        card.appendChild(prodPrec);
+        card.appendChild(prodPant);
+        card.appendChild(prodProc);
+        card.appendChild(prodCpu);
+        card.appendChild(prodGraf);
+        card.appendChild(boton);
+
+
     });
   })
   .catch(function(error) {
